67c989707195d0576989aa102531b6a26b4917df
Maria <m.gosp1704@gmail.com>
2024-12-26 16:48:07 +0200

    add task solution

---
+++ src/components/PersonLink.tsx
@@ -0,0 +1,23 @@
+import { Person } from '../types';
+import { Link } from 'react-router-dom';
+import cn from 'classnames';
+
+type Props = {
+  person: Person | undefined;
+};
+
+export const PersonLink: React.FC<Props> = props => {
+  const { person } = props;
+  // if (!person) {
+  //   return <span>-</span>
+  // }
+
+  return (
+    <Link
+      to={`/people/${person?.slug}`}
+      className={cn({ 'has-text-danger': person?.sex === 'f' })}
+    >
+      {person?.name}
+    </Link>
+  );
+};
